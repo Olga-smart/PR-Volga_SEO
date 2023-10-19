36d9c3369801b34e0aad181d05de9bcab61ce545
Olga_Tarasova <olyaaasyaaa@gmail.com>
2023-10-19 16:29:48 +0700

    Make header

---
+++ main.js
@@ -0,0 +1,26 @@
+class Header {
+  constructor(component) {
+    this._initFields(component);
+    this._attachEventHandlers();
+  }
+
+  _initFields(component) {
+    this._component = component;
+    this._burger = component.querySelector('.js-header__burger');
+    this._menu = component.querySelector('.js-header__menu');
+  }
+
+  _handleBurgerClick() {
+    this._burger.classList.toggle('header__burger_menu_open');
+    this._menu.classList.toggle('header__menu_open');
+  }
+
+  _attachEventHandlers() {
+    this._burger.addEventListener('click', this._handleBurgerClick.bind(this));
+  }
+}
+
+const header = document.querySelector('.js-header');
+if (header) {
+  new Header(header);
+}
